9589030e97486a75fe7b1c706093d20eb72600a7
Nick Nash <nicknash@Nicks-MacBook-Pro-2.local>
2017-02-13 16:00:31 -0600

    updated js files and gruntfile.js

--- javascripts/fbGetter.js
+++ javascripts/fbGetter.js
@@ -5,9 +5,9 @@
 // Our firebase URL address , we can make calls 
 function getKey() {
 	return {
-		apiKey: "",
-		authDomain: "",
-		databaseURL: ""
+		apiKey: "AIzaSyDJ-P7PbkEX_MVgVob00hPKzKJ03laRiUA",
+		authDomain: "movie-history-70b20.firebaseapp.com",
+		databaseURL: "https://movie-history-70b20.firebaseio.com"
 	};
 }
 
--- javascripts/main.js
+++ javascripts/main.js
@@ -1,3 +1,9 @@
 "use strict";
 
+let firebase = require("./config.js");
+let getKey = require("./fbGetter.js");
 
+var fb = Object.keys(getKey);
+
+
+console.log("firebase: ", fb);
