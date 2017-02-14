32396ac7bd5b1c4e4c715d0cb7b67b877a93cf22
Dean Smith <Dean Smith>
2017-02-14 16:07:54 -0600

    empty functions and stuff

--- javascripts/dbInteraction.js
+++ javascripts/dbInteraction.js
@@ -1,1 +1,23 @@
 "use strict";
+//We made empty stuff for us to use later
+
+// how we search the movie database and return results
+function searchAPI() {
+
+}
+// how we search the users list of movies
+function searchDb() {
+
+}
+
+function addToTracked() {
+
+}
+
+function addToUnwatched() {
+
+}
+
+function rateMovie() {
+
+}
